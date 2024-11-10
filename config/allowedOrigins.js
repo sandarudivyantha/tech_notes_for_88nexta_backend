d2e984c6ce5d4c78aaee2ecd7138b0c6dd96a18b
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-11-10 19:41:20 +0530

    origins changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://tech-notes-for-88nexta-backend.vercel.app'
+    'https://tech-notes-for-88nexta-backend.vercel.app',
+    'https://tech-notes-for-88nexta-backend-7hmi0ixd5.vercel.app/'
 ]
 
 module.exports = allowedOrigins
